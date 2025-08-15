bd7c0ad62a51eedda60260395981ff5b5eccaa17
Jah-Ai <batteringmoms@gmail.com>
2025-08-15 15:37:17 -0400

    Removed Python Added Absolute Path to env Variable

--- BashScriptBasics.sh
+++ BashScriptBasics.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 #Shebang line - Cleaner
 
+# In Terminal - export PATH="/home/kali/dirsearch:$PATH"
+# Absolute Path to dirsearch as listed above
+
 nmap $1
-python3 /home/kali/dirsearch/dirsearch.py -u $1 -e php
+dirsearch.py -u $1 -e php
+#Change to Absolute PATH to dirsearch
